5a79690401467622a459ea45974a1f002bacd89c
Erik <872286926@qq.com>
2018-07-09 22:02:27 +0800

    add files

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    {{ totalPrice }}
+    <Apple></Apple>
+    <Banana></Banana>
+  </div>
+</template>
+
+<script>
+import Apple from './components/apple'
+import Banana from './components/banana'
+export default {
+  name: 'App',
+  components: {
+    Apple,
+    Banana
+  },
+  data: function () {
+    return { }
+  },
+  computed: {
+    totalPrice () {
+      return this.$store.state.totalPrice
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,37 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuex from 'vuex'
+
+Vue.config.productionTip = false
+Vue.use(Vuex)
+
+let store = new Vuex.Store({
+  state: {
+    totalPrice: 0
+  },
+  mutations: {
+    increment (state, price) {
+      state.totalPrice += price
+    },
+    decrement (state, price) {
+      state.totalPrice -= price
+    }
+  },
+  actions: {
+    increment (content, price) {
+      content.commit('increment', price)
+    }
+  }
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
